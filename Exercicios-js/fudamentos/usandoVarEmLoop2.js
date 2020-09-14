47acb3903263afa737fb714f818d18baff031b6f
Carlos Legnaro <carlos_legnaro@outlook.com>
2020-09-14 09:44:43 -0300

    Usuando Var em loop

--- Exercicios-js/fudamentos/usandoVarEmLoop2.js
+++ Exercicios-js/fudamentos/usandoVarEmLoop2.js
@@ -1,5 +1,11 @@
-for (let i= 0; i < 10; i++)
-{
-    console.log(i)
+const funcs = []
+
+for (var i= 0; i < 10; i++){
+    funcs.push(function()
+    {
+        console.log(i)
+    })
 }
-console.log('i =', i)
+
+funcs[2]()
+funcs[8]()
